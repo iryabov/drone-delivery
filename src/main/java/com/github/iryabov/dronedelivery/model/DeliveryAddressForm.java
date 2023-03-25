ddd6c2fcbe54c61ad9c1eee0925525989ea099fb
iryabov <to.iryabov@gmail.com>
2023-03-25 10:12:05 +0100

    Change location coordinate limits

--- src/main/java/com/github/iryabov/dronedelivery/model/DeliveryAddressForm.java
+++ src/main/java/com/github/iryabov/dronedelivery/model/DeliveryAddressForm.java
@@ -14,11 +14,11 @@ public class DeliveryAddressForm {
     @Schema(description = "Delivery address", example = "бул. Драган Цанков 36, София, Болгария")
     private String address;
     @Schema(description = "Latitude of destination", example = "42.67034")
-    @Min(value = -90, message = "Latitude cannot be less than -90")
-    @Max(value = 90, message = "Latitude cannot be greater than +90")
+    @Min(value = -180, message = "Latitude cannot be less than -180")
+    @Max(value = 180, message = "Latitude cannot be greater than +180")
     private double latitude;
     @Schema(description = "Longitude of destination", example = "23.35111")
-    @Min(value = -90, message = "Longitude cannot be less than -90")
-    @Max(value = 90, message = "Longitude cannot be greater than +90")
+    @Min(value = -180, message = "Longitude cannot be less than -180")
+    @Max(value = 180, message = "Longitude cannot be greater than +180")
     private double longitude;
 }
